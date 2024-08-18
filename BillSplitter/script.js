737ebf2bd4e0edcd558ee2a201fd89fe4828958f
przemekjoniec <joniecprzemek098@gmail.com>
2024-08-18 21:01:37 +0200

    added new project

---
+++ BillSplitter/script.js
@@ -0,0 +1,27 @@
+const price = document.querySelector('#price')
+const people = document.querySelector('#people')
+const tip = document.querySelector('#tip')
+const error = document.querySelector('.error')
+const costInfo = document.querySelector('.cost-info')
+const cost = document.querySelector('.cost')
+const countBtn = document.querySelector('.count')
+
+const showBill = () => {
+	if (price.value == '' || people.value == '' || tip.value == 0) {
+		error.textContent = 'Fill out the form!'
+		costInfo.style.display = 'none'
+	} else {
+		error.textContent = ''
+		countBill()
+	}
+}
+const countBill = () => {
+	const newPrice = parseFloat(price.value)
+	const newPeople = parseInt(people.value)
+	const newTip = parseFloat(tip.value)
+
+	const sum = (newPrice + newPrice * newTip) / newPeople
+	costInfo.style.display = 'block'
+	cost.textContent = sum.toFixed(2)
+}
+countBtn.addEventListener('click', showBill)
